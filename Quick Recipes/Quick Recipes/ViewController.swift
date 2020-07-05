9be18c05ecb208e59f6bb2e6093e5f955dcf3ce7
Leand Thaqi <44322393+LeandThaqi@users.noreply.github.com>
2020-07-05 12:49:00 -0700

    Init Commit
    Init

---
+++ Quick Recipes/Quick Recipes/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Quick Recipes
+//
+//  Created by Leand Thaqi on 5.7.20.
+//  Copyright © 2020 Leand Thaqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var imageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "background_main.png")
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.insertSubview(imageView, at: 0)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
